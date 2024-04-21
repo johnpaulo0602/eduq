f7d188be16175fcd307356a625672e3ab657c512
JOHN PAULO SILVA PAIVA <johnpaulo0602@gmail.com>
2024-04-21 13:57:44 -0300

    Sidebar component

--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -1,144 +1,109 @@
 'use client';
-import PerfectScrollbar from 'react-perfect-scrollbar';
 import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 import { toggleSidebar } from '@/store/themeConfigSlice';
 import { IRootState } from '@/store';
-import { useState, useEffect } from 'react';
-import IconCaretsDown from '@/components/icon/icon-carets-down';
+import { useEffect, useState } from 'react';
 import IconMenuDashboard from '@/components/icon/menu/icon-menu-dashboard';
 import IconSettings from '../icon/icon-settings';
 import IconMenuTodo from '@/components/icon/menu/icon-menu-todo';
 import IconMenuElements from '@/components/icon/menu/icon-menu-elements';
 import IconMenuCharts from '@/components/icon/menu/icon-menu-charts';
 import IconMenuUsers from '@/components/icon/menu/icon-menu-users';
+import IconCaretsDown from '@/components/icon/icon-carets-down';
 import { usePathname } from 'next/navigation';
-import { getTranslation } from '@/i18n';
+import AnimateHeight from 'react-animate-height';
+
+const menuItems = [
+    { href: "/", icon: IconMenuDashboard, label: "Home" },
+    { href: "/apps/mailbox", icon: IconMenuElements, label: "Questões" },
+    { href: "/apps/todolist", icon: IconMenuTodo, label: "Provas" },
+    { href: "/apps/notes", icon: IconMenuCharts, label: "Relatórios" },
+    { href: "/apps/scrumboard", icon: IconMenuUsers, label: "Usuários" },
+    { href: "/apps/contacts", icon: IconSettings, label: "Configurações" },
+    { href: "/apps/contacts", icon: IconSettings, label: "Configurações" },
+    { href: "/apps/contacts", icon: IconSettings, label: "Configurações" },
+    { href: "/apps/contacts", icon: IconSettings, label: "Configurações" },
+    { href: "/apps/contacts", icon: IconSettings, label: "Configurações" },
+    {
+        href: "#", icon: IconMenuDashboard, label: "Dashboard", submenu: [
+            { href: "/sales", label: "Vendas" },
+            { href: "/analytics", label: "Análises" },
+            { href: "/finance", label: "Finanças" },
+            { href: "/crypto", label: "Cripto" },
+        ]
+    },
+];
 
 const Sidebar = () => {
     const dispatch = useDispatch();
-    const { t } = getTranslation();
     const pathname = usePathname();
-    const [currentMenu, setCurrentMenu] = useState<string>('');
-    const [errorSubMenu, setErrorSubMenu] = useState(false);
     const themeConfig = useSelector((state: IRootState) => state.themeConfig);
     const semidark = useSelector((state: IRootState) => state.themeConfig.semidark);
-    const toggleMenu = (value: string) => {
-        setCurrentMenu((oldValue) => {
-            return oldValue === value ? '' : value;
-        });
-    };
+    const [currentMenu, setCurrentMenu] = useState<string | null>(null);
 
     useEffect(() => {
-        const selector = document.querySelector('.sidebar ul a[href="' + window.location.pathname + '"]');
-        if (selector) {
-            selector.classList.add('active');
-            const ul: any = selector.closest('ul.sub-menu');
-            if (ul) {
-                let ele: any = ul.closest('li.menu').querySelectorAll('.nav-link') || [];
-                if (ele.length) {
-                    ele = ele[0];
-                    setTimeout(() => {
-                        ele.click();
-                    });
-                }
-            }
-        }
-    }, []);
-
-    useEffect(() => {
-        setActiveRoute();
         if (window.innerWidth < 1024 && themeConfig.sidebar) {
             dispatch(toggleSidebar());
         }
-    }, [pathname]);
+    }, [pathname, themeConfig.sidebar, dispatch]);
 
-    const setActiveRoute = () => {
-        let allLinks = document.querySelectorAll('.sidebar ul a.active');
-        for (let i = 0; i < allLinks.length; i++) {
-            const element = allLinks[i];
-            element?.classList.remove('active');
+    const isActive = (href: string) => pathname === href;
+
+    const toggleMenu = (menuLabel: string) => {
+        if (currentMenu === menuLabel) {
+            setCurrentMenu(null);
+        } else {
+            setCurrentMenu(menuLabel);
         }
-        const selector = document.querySelector('.sidebar ul a[href="' + window.location.pathname + '"]');
-        selector?.classList.add('active');
     };
 
     return (
         <div className={semidark ? 'dark' : ''}>
-            <nav
-                className={`sidebar fixed bottom-0 top-0 z-50 h-full min-h-screen w-[260px] shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] transition-all duration-300 ${semidark ? 'text-white-dark' : ''}`}
-            >
+            
+            <nav className={`sidebar fixed bottom-0 top-0 z-50 h-full min-h-screen w-[260px] shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] transition-all duration-300 ${semidark ? 'text-white-dark' : ''}`}>
+                <div className="flex items-center justify-between px-4 py-3 bg-white dark:bg-black">
+                    <Link href="/" className="main-logo flex shrink-0 items-center">
+                        <img className="ml-[5px] w-24" src="/assets/images/logo.svg" alt="logo" />
+                    </Link>
+
+                    <button
+                        type="button"
+                        className="collapse-icon flex h-8 w-8 items-center rounded-full transition duration-300 hover:bg-gray-500/10 rtl:rotate-180 dark:text-white-light dark:hover:bg-dark-light/10"
+                        onClick={() => dispatch(toggleSidebar())}
+                    >
+                        <IconCaretsDown className="m-auto rotate-90" />
+                    </button>
+                </div>
                 <div className="h-full bg-white dark:bg-black">
-                    <div className="flex items-center justify-between px-4 py-3">
-                        <Link href="/" className="main-logo flex shrink-0 items-center">
-                            <img className="ml-[5px] w-24" src="/assets/images/logo.svg" alt="logo" />
-                        </Link>
-
-                        <button
-                            type="button"
-                            className="collapse-icon flex h-8 w-8 items-center rounded-full transition duration-300 hover:bg-gray-500/10 rtl:rotate-180 dark:text-white-light dark:hover:bg-dark-light/10"
-                            onClick={() => dispatch(toggleSidebar())}
-                        >
-                            <IconCaretsDown className="m-auto rotate-90" />
-                        </button>
-                    </div>
-                    <PerfectScrollbar className="relative h-[calc(100vh-80px)]">
-                        <ul className="relative space-y-0.5 p-4 py-0 font-semibold">
-                            <li className="nav-item">
-                                <ul>
-                                    <li className="nav-item">
-                                        <Link href="/" className="group">
-                                            <div className="flex items-center">
-                                                <IconMenuDashboard className="shrink-0 group-hover:!text-primary" />
-                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Home</span>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link href="/apps/mailbox" className="group">
-                                            <div className="flex items-center">
-                                                <IconMenuElements className="shrink-0 group-hover:!text-primary" />
-                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Questões</span>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link href="/apps/todolist" className="group">
-                                            <div className="flex items-center">
-                                                <IconMenuTodo className="shrink-0 group-hover:!text-primary" />
-                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Provas</span>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link href="/apps/notes" className="group">
-                                            <div className="flex items-center">
-                                                <IconMenuCharts className="shrink-0 group-hover:!text-primary" />
-                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Relatórios</span>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link href="/apps/scrumboard" className="group">
-                                            <div className="flex items-center">
-                                                <IconMenuUsers className="shrink-0 group-hover:!text-primary" />
-                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Usuários</span>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link href="/apps/contacts" className="group">
-                                            <div className="flex items-center">
-                                                <IconSettings className="shrink-0 group-hover:!text-primary" />
-                                                <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">Configurações</span>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                </ul>
+                    <ul className="relative space-y-0.5 p-4 py-0 font-semibold">
+                        {menuItems.map(({ href, icon: Icon, label, submenu }) => (
+                            <li key={href} className={`nav-item ${submenu ? 'menu' : ''}`}>
+                                <button type="button" className={`group ${isActive(href) ? 'active' : ''}`} onClick={() => submenu && toggleMenu(label)}>
+                                    <div className="flex items-center">
+                                        <Icon className="shrink-0 group-hover:!text-primary" />
+                                        <span className={`text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark`}>{label}</span>
+                                    </div>
+                                    {submenu && (
+                                        <div className={`${currentMenu !== label ? '-rotate-90 rtl:rotate-90' : ''}`}>
+                                            <IconCaretsDown />
+                                        </div>
+                                    )}
+                                </button>
+                                {submenu && (
+                                    <AnimateHeight duration={300} height={currentMenu === label ? 'auto' : 0}>
+                                        <ul className="sub-menu text-gray-500">
+                                            {submenu.map(subItem => (
+                                                <li key={subItem.href}>
+                                                    <Link href={subItem.href}>{subItem.label}</Link>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </AnimateHeight>
+                                )}
                             </li>
-
-                        </ul>
-                    </PerfectScrollbar>
+                        ))}
+                    </ul>
                 </div>
             </nav>
         </div>
