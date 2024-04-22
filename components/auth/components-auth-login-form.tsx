e886485e8062e4d068d8565905f31ed806e6fadd
JOHN PAULO SILVA PAIVA <johnpaulo0602@gmail.com>
2024-04-22 05:14:12 -0300

    update ComponentsAuthLoginForm

--- components/auth/components-auth-login-form.tsx
+++ components/auth/components-auth-login-form.tsx
@@ -16,15 +16,24 @@ const ComponentsAuthLoginForm = () => {
             <div>
                 <label htmlFor="Email">Email</label>
                 <div className="relative text-white-dark">
-                    <input id="Email" type="email" placeholder="Enter Email" className="form-input ps-10 placeholder:text-white-dark" />
+                    <input id="Email" type="email" placeholder="Digite seu email" className="form-input ps-10 placeholder:text-white-dark" />
                     <span className="absolute start-4 top-1/2 -translate-y-1/2">
                         <IconMail fill={true} />
                     </span>
                 </div>
             </div>
+            <div>
+                <label htmlFor="Password">Password</label>
+                <div className="relative text-white-dark">
+                    <input id="Password" type="password" placeholder="Digite sua senha" className="form-input ps-10 placeholder:text-white-dark" />
+                    <span className="absolute start-4 top-1/2 -translate-y-1/2">
+                        <IconLockDots fill={true} />
+                    </span>
+                </div>
+            </div>
 
-            <button type="submit" className="btn btn-gradient !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]">
-                Link Magico
+            <button type="submit" className="btn btn-primary !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(39,66,213,0.44)]">
+                Entrar
             </button>
         </form>
     );
